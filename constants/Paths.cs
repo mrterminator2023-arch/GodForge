0f8a59f7c324302a59d1f0f5dacdc7f60704647a
Idel Nigmatullin <274504072+mrterminator2023-arch@users.noreply.github.com>
2026-09-06 12:04:25 +0500

    Fix Paths static init order (CompilerPackPath before PublicizedAssemblyPath)

--- constants/Paths.cs
+++ constants/Paths.cs
@@ -56,16 +56,17 @@ public static class Paths
     /// </summary>
     public static readonly string NMLCommitPath = Combine(NMLPath, "commit");
 
+    /// <summary>
+    /// Optional compiler pack folder (Roslyn + Assembly-CSharp-Publicized), needed only for source mods
+    /// </summary>
+    public static readonly string CompilerPackPath = Combine(NMLPath, "Compiler");
+
+
 
     /// <summary>
     /// Path to the publicized Assembly-CSharp.dll file, on android this is used as IL replacements for transpiler support
     /// </summary>
     public static readonly string PublicizedAssemblyPath = Combine(CompilerPackPath, "Assembly-CSharp-Publicized.dll");
-
-    /// <summary>
-    /// Optional compiler pack folder (Roslyn + Assembly-CSharp-Publicized), needed only for source mods
-    /// </summary>
-    public static readonly string CompilerPackPath = Combine(NMLPath, "Compiler");
 
     /// <summary>
     /// Path to folder mods config under persistent data folder
